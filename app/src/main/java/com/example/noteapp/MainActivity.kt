08d2d09386b43dbc2b31049a10084276a894369c
Aleke89 <alnur.rysbek@mail.ru>
2023-03-17 15:13:04 +0600

    SecondCommitNoteApp

--- app/src/main/java/com/example/noteapp/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -1,11 +1,127 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.noteapp
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Note
+import android.view.MenuItem
+import android.widget.LinearLayout
+import android.widget.PopupMenu
+import android.widget.SearchView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.cardview.widget.CardView
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.noteapp.adapter.NotesAdapter
+import com.example.noteapp.database.NoteDatabase
+import com.example.noteapp.databinding.ActivityMainBinding
+import com.example.noteapp.models.NoteViewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),NotesAdapter.NotesClickListener,PopupMenu.OnMenuItemClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var database: NoteDatabase
+    lateinit var viewModel: NoteViewModel
+    lateinit var adapter: NotesAdapter
+    lateinit var selectedNode: com.example.noteapp.models.Note
+    private val updateNote =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val note =
+                    result.data?.getSerializableExtra("note") as? com.example.noteapp.models.Note
+                if (note != null) {
+                    viewModel.updateNote(note)
+                }
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Initializing the UI
+        initUi()
+        viewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application)
+        ).get(NoteViewModel::class.java)
+
+        viewModel.allnotes.observe(this) { list ->
+
+            list?.let {
+                adapter.updateList(list)
+            }
+
+        }
+        database = NoteDatabase.getDatabase(this)
+    }
+
+    private fun initUi() {
+        binding.recyclerView.setHasFixedSize(true)
+        binding.recyclerView.layoutManager = StaggeredGridLayoutManager(2, LinearLayout.VERTICAL)
+        adapter = NotesAdapter(this, this)
+        binding.recyclerView.adapter = adapter
+        val getContent =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val note =
+                        result.data?.getSerializableExtra("note") as? com.example.noteapp.models.Note
+                    if (note != null) {
+                        viewModel.insertNote(note)
+                    }
+                }
+            }
+
+        binding.fbNoteAdd.setOnClickListener {
+            val intent = Intent(this, NoteAddActivity::class.java)
+            getContent.launch(intent)
+
+        }
+
+
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText != null) {
+                    adapter.filterList(newText)
+                }
+
+                return true
+            }
+
+        })
+    }
+
+    override fun onItemClick(note: com.example.noteapp.models.Note) {
+        val intent = Intent(this@MainActivity,NoteAddActivity::class.java)
+        intent.putExtra("current_note",note)
+        updateNote.launch(intent)
+    }
+
+    override fun onLongItemClicked(note: com.example.noteapp.models.Note, cardView: CardView) {
+        selectedNode = note
+        popUpDisplay(cardView)
+    }
+
+    private fun popUpDisplay(cardView: CardView) {
+        val popup = PopupMenu(this,cardView)
+        popup.setOnMenuItemClickListener(this@MainActivity)
+        popup.inflate(R.menu.pop_up_menu)
+        popup.show()
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        if(item?.itemId == R.id.delete_note){
+            viewModel.deleteNote(selectedNode)
+            return true
+        }
+        return false
     }
 }
--- app/src/main/java/com/example/noteapp/NoteAddActivity.kt
+++ app/src/main/java/com/example/noteapp/NoteAddActivity.kt
@@ -1,11 +1,63 @@
 package com.example.noteapp
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.noteapp.databinding.ActivityNoteAddBinding
+import com.example.noteapp.models.Note
+import java.text.SimpleDateFormat
+import java.util.*
 
 class NoteAddActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityNoteAddBinding
+    private lateinit var note: com.example.noteapp.models.Note
+    private lateinit var old_note: com.example.noteapp.models.Note
+    var isUpdated = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_note_add)
+        binding = ActivityNoteAddBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        try {
+            old_note =
+                intent.getSerializableExtra("current_note") as com.example.noteapp.models.Note
+            binding.etTitle.setText(old_note.title)
+            binding.etNote.setText(old_note.note)
+            isUpdated = true
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        binding.imgCheck.setOnClickListener {
+            val title = binding.etTitle.text.toString()
+            val note_desc = binding.etNote.text.toString()
+
+            if (title.isNotEmpty() || note_desc.isNotEmpty()) {
+                val formatter = SimpleDateFormat("EEE,d MMM yyyy HH:mm a")
+                if (isUpdated) {
+                    note = Note(old_note.id, title, note_desc, formatter.format(Date())
+                    )
+                } else {
+                    note = Note(null, title, note_desc, formatter.format(Date()))
+                }
+
+                val intent = Intent()
+                intent.putExtra("note",note)
+                setResult(Activity.RESULT_OK,intent)
+                finish()
+
+            }else{
+                Toast.makeText(this@NoteAddActivity,"Please enter all data",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+        }
+        binding.imgBack.setOnClickListener {
+            onBackPressed()
+        }
+
     }
 }
